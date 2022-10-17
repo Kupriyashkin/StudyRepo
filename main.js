62cc199d5681b1def1d55ac8f30211ee7f7f9064
Kupriyashkin <kupriyashkin2001@gmail.com>
2022-10-17 19:26:05 +0300

    project init

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*let i=0
+ while (i<2) {
+     alert(i)
+    i++
+}*/
+
+/*for (let i = 0; i < 3; i++) {
+     alert(i);
+    }*/
+
+/*let sum = 0;
+while (true) {
+    let value = +prompt("Введите число", '');
+    if (!value) break;
+sum += value;
+}
+alert( 'Сумма: ' + sum );*/
+
+/*for (let i = 2; i<11;i++) {
+    alert (i)
+}*/
+
+/*n = +prompt("Number", '')
+nextPrime:
+    for(let i = 2; i<n; i++) {
+        for (let j = 2; j<n; j++) {
+            if(i%j == 0) continue nextPrime;
+}
+alert(n);
+}*/
+console.log('hello')
+
