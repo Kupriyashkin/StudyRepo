d531ae90b0bdf2fbf8f7e0536b4a8b863fdfad13
Kupriyashkin <kupriyashkin2001@gmail.com>
2022-10-19 21:28:13 +0300

    func Expression and Arrow funct

--- main.js
+++ main.js
@@ -65,3 +65,42 @@ console.log(min(5,6))*/
     return (age>18) ? true : confirm('?')
     return (age>18) || confirm('?')
 }*/
+
+
+//Function Expression
+
+/*let sayHi = function() {
+    alert("Hi");
+};*/
+
+/*let age = +promt("How old are you?",'')
+let welcome
+if (age<18) {
+    welcome = function() {
+        alert("Hi");
+    };
+} else {
+    welcome = function() {
+        alert("Good morning");
+    };
+}
+welcome()
+
+
+//стрелочные функции //Arrow Function
+
+/*let func = (arg1, arg2, argN) => expression;
+let funct = function(arg1, arg2, argN) {
+    return expression;
+}*/
+/*let sum = (a,b) => {
+    let result = a+b;
+    return result;
+};*/
+/*function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+ask("Вы согласны?",
+() => console.log('Good'),
+() => console.log('Hmm...'));*/
